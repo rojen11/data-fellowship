44c71dbfadf00bd22870bce18fd367ac9fcb73b7
ishandongol <ishandongol@gmail.com>
2021-02-12 17:12:37 +0000

    Deploying to gh-pages from  @ da6849d8332545097210846b9d0d92eb8bdfe78b 🚀

---
+++ _next/static/D8TkSaHp-ir5jpGYAcdxU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fb660d8c921e9fb69f2c6.css","static\u002Fchunks\u002Fpages\u002Findex-c1300dc0a9a6b23804a1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-daf52341da019c28224d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
